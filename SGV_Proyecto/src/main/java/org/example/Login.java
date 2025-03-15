a42b2788e1cc738292662c80fce1d6d94626f07f
Andres <naschlol88@gmail.com>
2025-03-15 11:47:35 -0600

    Cambio importante interfaz de login e inventario
    Cree ka interfaz del login y de inventario, lo que nos falta es hacer que login pueda recorrer una lista para ver distintos usuarios e inventario pueda tambien recorrer una lista para ver la informacion de los inventarios, y alimentar el main

---
+++ SGV_Proyecto/src/main/java/org/example/Login.java
@@ -0,0 +1,62 @@
+package org.example;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class Login extends JFrame {
+    private JPanel Login;
+    private JTextField UsuarioTXT;
+    private JPasswordField ContraseñaTXT;
+    private JLabel UsuarioLB;
+    private JLabel ContraseñaLB;
+    private JButton LoginBtn;
+    private JButton SalirBtn;
+    private boolean autenticado = false;
+
+    //1- constructor inicializa la clase
+    public Login(){
+        //Los siguientes metodos vienen de Jframe, no hay que incializar, son heredados
+
+        setContentPane(Login); //Main panel del Java Swing, contenedor
+        setTitle("Login"); //Nombre de la Ventana
+        setDefaultCloseOperation(EXIT_ON_CLOSE); //cierre la ventana mediante alguna funcionalidad
+        setSize(400,400); //Tamaño de la ventana
+        setVisible(true); //ventana activa o inactiva
+
+        //PRIMER EVENTO click en login botton (con usuario y contraseña)
+        LoginBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                //Aca va_todo lo que se ejecuta a la hora del click
+                String user = UsuarioTXT.getText();
+                String password = ContraseñaTXT.getText();
+
+                //Validacion del login
+                if(user.equals("finanzas1")&& password.equals("1234")){
+                    JOptionPane.showMessageDialog(null,"Ingreso Correcto, Bienvenido!");
+                    autenticado = true;
+                    //cerramos la ventana del menu
+                    dispose();
+                }else{
+                    JOptionPane.showMessageDialog(null,"Verifique sus Credenciales");
+                    autenticado = false;
+                }
+            }
+        });
+
+        //SEGUNDO EVENTO Salir de la interfaz
+        SalirBtn.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                dispose();
+            }
+        });
+
+    }
+    // Método_para obtener el estado de autenticación
+    public boolean esAutenticado() {
+        return autenticado;
+    }
+
+}
--- SGV_Proyecto/src/main/java/org/example/Principal_SGV.java
+++ SGV_Proyecto/src/main/java/org/example/Principal_SGV.java
@@ -1,10 +1,26 @@
 package org.example;
 //prueba repo
 
+import javax.swing.*;
 import java.util.Date;
 
 public class Principal_SGV {
     public static void main(String[] args) {
+
+        Login login = new Login(); // llamamos al login
+
+        // Esperamos hasta que el login sea exitoso
+        while (!login.esAutenticado()) { //la variable en login debe cambiar a true
+            // Continuar mostrando la ventana de login hasta que el usuario se autentique
+            try {
+                Thread.sleep(500); // Esperamos un poco antes de volver a revisar el estado
+            } catch (InterruptedException e) {
+                //e.printStackTrace();
+            }
+        }
+
+        // Si la autenticación es correcta, pasamos al menú
+        System.out.println("Login exitoso. Iniciando el sistema...");
 
         //Crear vehiculos
         Automovil auto = new Automovil("Toyota","Corolla","Negro",2020,23800,4,true);
